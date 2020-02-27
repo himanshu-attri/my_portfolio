3771042090897c1033d5f1578cbf6c05b7ced5c7
Himanshu Attri <himanshuattri.ha@gmail.com>
2020-02-27 12:05:24 +0530

    Updates

---
+++ precache-manifest.d3e3f13983e49efb032a6a5c32d401a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4801b35d080762bb4ab53b5238215970",
+    "url": "/my_portfolio/index.html"
+  },
+  {
+    "revision": "041430c823e3a4d44a1b",
+    "url": "/my_portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a033688861e7829588aa",
+    "url": "/my_portfolio/static/js/2.ac8457c8.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/my_portfolio/static/js/2.ac8457c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "041430c823e3a4d44a1b",
+    "url": "/my_portfolio/static/js/main.f23b104e.chunk.js"
+  },
+  {
+    "revision": "cd21e0e5de7a84d25623",
+    "url": "/my_portfolio/static/js/runtime-main.c196de3b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my_portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
